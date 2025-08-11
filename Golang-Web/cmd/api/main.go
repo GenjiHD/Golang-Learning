1b446ef65d999de4249c3b7141d56cd3316d5031
GenjiHD <joaquincastrosarmientosanchez@gmail.com>
2025-08-11 15:50:42 -0700

    back-end web basics ended

--- Golang-Web/cmd/api/main.go
+++ Golang-Web/cmd/api/main.go
@@ -11,10 +11,10 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/Golang-Web?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONSS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONSS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
 
@@ -27,6 +27,9 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
+	defer db.Close()
+	log.Println("database connection pool established")
 
 	store := store.NewStorage(db)
 
